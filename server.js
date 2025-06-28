118b23957ea1e726522dfecc05bc5d5f1e6aca91
Kavyakapoor <167117026+kavyakapoor420@users.noreply.github.com>
2025-06-28 15:39:00 +0530

    Integrated Aptos with our Backend Node Server

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const axios = require("axios");
 const { Aptos } = require("@aptos-labs/ts-sdk");
 
+//created node backend completed backedn code interagret Aptos init 
+
 const app = express();
 const PORT = 3000;
 
